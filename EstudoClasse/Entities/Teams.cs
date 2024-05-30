5b47ad4721f339ca8c83db6daf2079ece676fe91
CaioAdania <caioadania@gmail.com>
2024-05-30 11:45:58 -0300

    estudos classe

---
+++ EstudoClasse/Entities/Teams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoClasse.Entities
+{
+    internal class Teams
+    {
+        public string Time;
+        public int Titulos;
+        public int Criacao;
+        public string Rival;
+        public double Torcedores;
+
+        public override string ToString()
+        {
+            return "Seu time de coração é "
+                + Time
+                + ", ele tem "
+                + Titulos
+                + " Titulos, sua criação foi em "
+                + Criacao
+                + ", tem uma torcida com aproximadamente "
+                + Torcedores
+                + " torcedores, e seu Rival é o "
+                + Rival;
+        }
+    }
+}
---
+++ EstudoClasse/Program.cs
@@ -0,0 +1,33 @@
+﻿using EstudoClasse.Entities;
+
+namespace EstudoClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teams clube = new Teams();
+            Console.Write("Qual seu time de coração? ");
+            clube.Time = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Quantos titulos tem seu time? ");
+            clube.Titulos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Qual o Ano de criação do seu time? ");
+            clube.Criacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Qual o número de torcedores do seu time? ");
+            clube.Torcedores = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Qual o rival do seu time? ");
+            clube.Rival = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(clube);
+        }
+    }
+}
